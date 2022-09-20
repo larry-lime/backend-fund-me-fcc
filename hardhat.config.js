95a8baee5b618253402988651a610a9c00384b28
Lawrence Lim <ll4715@nyu.edu>
2022-09-20 17:03:27 +0800

    Updated private key to work without .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ require('hardhat-deploy')
 
 const RINKEBY_RPC_URL =
   process.env.RINKEBY_RPC_URL || 'https://eth-rinkeby/example'
-const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0x756c10f614795972a3d269eefdea3ee32d557065297f08bd6d0a7f30db04eebd'
 const COINMKTCAP_API_KEY = process.env.COINMKTCAP_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
 
@@ -20,12 +20,12 @@ module.exports = {
       chainId: 31337,
       // gasPrice: 130000000000,
     },
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 4,
-      blockConfirmations: 6,
-    },
+    // rinkeby: {
+    //   url: RINKEBY_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 4,
+    //   blockConfirmations: 6,
+    // },
   },
   solidity: {
     compilers: [{ version: '0.8.8' }, { version: '0.6.6' }],
